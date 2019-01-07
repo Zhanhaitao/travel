17aecbc9edcc993f1c90239788d731c36281d39b
zhanhaitao <13564420419@163.com>
2019-01-07 10:11:48 +0800

    list

---
+++ src/pages/city/city.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <city-header></city-header><city-search></city-search><city-list></city-list><city-alphabet></city-alphabet>
+  </div>
+</template>
+
+<script>
+import cityHeader from './components/header';
+import citySearch from './components/search';
+import cityList from './components/list';
+import cityAlphabet from './components/alphabet';
+export default {
+  name: 'City',
+  components: {
+    cityHeader,
+    citySearch,
+    cityList,
+    cityAlphabet
+  }
+};
+</script>
+
+<style lang="stylus" scoped></style>
